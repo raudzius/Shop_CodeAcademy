d6f76a567b528d51eae902057dedb9974d58c3fc
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-01 15:18:13 +0300

    chore(1): move custom size checkbox file

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -11,7 +11,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CustomSizeCheckbox from './CustomSizeCheckbox';
+import CustomSizeCheckbox from './form-controls/CustomSizeCheckbox';
 import Copyright from './Copyright';
 
 const theme = createTheme();
