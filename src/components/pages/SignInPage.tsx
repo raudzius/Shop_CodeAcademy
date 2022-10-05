8c24edc69d295c15c7d46bf076feba3a342e7522
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-05 09:09:11 +0300

    chore(3,5): move pages to pages folder

--- src/components/pages/SignInPage.tsx
+++ src/components/pages/SignInPage.tsx
@@ -11,8 +11,8 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CustomSizeCheckbox from './form-controls/CustomSizeCheckbox';
-import Copyright from './Copyright';
+import CustomSizeCheckbox from '../form-controls/CustomSizeCheckbox';
+import Copyright from '../Copyright';
 
 const theme = createTheme();
 
