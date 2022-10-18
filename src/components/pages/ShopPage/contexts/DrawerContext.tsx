8386b60cad7522edc02baa9a1d8b4e0b22899f07
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-18 10:19:42 +0300

    feat(14): write context provider in context file for better concept segregation

--- src/components/pages/ShopPage/contexts/DrawerContext.tsx
+++ src/components/pages/ShopPage/contexts/DrawerContext.tsx
@@ -8,4 +8,23 @@ type DrawerContextType = {
 
 const DrawerContext = React.createContext<DrawerContextType>({} as DrawerContextType);
 
+type DrawerProviderProps = {
+  children: React.ReactNode;
+};
+
+export const DrawerProvider: React.FC<DrawerProviderProps> = ({ children }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const drawerContextValue = React.useMemo(
+    () => ({
+      open,
+      openDrawer: () => setOpen(true),
+      closeDrawer: () => setOpen(false),
+    }),
+    [open],
+  );
+
+  return <DrawerContext.Provider value={drawerContextValue}>{children}</DrawerContext.Provider>;
+};
+
 export default DrawerContext;
--- src/components/pages/ShopPage/index.tsx
+++ src/components/pages/ShopPage/index.tsx
@@ -4,29 +4,16 @@ import { Box } from '@mui/material';
 import ApplicationBar from './components/ApplicationBar';
 import Sidebar from './components/Sidebar';
 import MainSection from './components/MainSection';
-import DrawerContext from './contexts/DrawerContext';
+import { DrawerProvider } from './contexts/DrawerContext';
 
-const ShopPage = () => {
-  const [open, setOpen] = React.useState(false);
-
-  const drawerContextValue = React.useMemo(
-    () => ({
-      open,
-      openDrawer: () => setOpen(true),
-      closeDrawer: () => setOpen(false),
-    }),
-    [open],
-  );
-
-  return (
-    <DrawerContext.Provider value={drawerContextValue}>
-      <Box sx={{ display: 'flex' }}>
-        <ApplicationBar />
-        <Sidebar />
-        <MainSection />
-      </Box>
-    </DrawerContext.Provider>
-  );
-};
+const ShopPage = () => (
+  <DrawerProvider>
+    <Box sx={{ display: 'flex' }}>
+      <ApplicationBar />
+      <Sidebar />
+      <MainSection />
+    </Box>
+  </DrawerProvider>
+);
 
 export default ShopPage;
