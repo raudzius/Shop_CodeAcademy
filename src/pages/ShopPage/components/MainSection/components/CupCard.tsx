f9a5a31b1ed930123738a51b4e61fd99389b2098
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-10-21 05:11:51 +0300

    feat(26): export swiper to separate component

--- src/pages/ShopPage/components/MainSection/components/CupCard.tsx
+++ src/pages/ShopPage/components/MainSection/components/CupCard.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import {
- Box, Grid, Paper, styled,
+ Box, Button, Grid, Paper, Typography,
 } from '@mui/material';
-import { Swiper as SwiperJS, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Pagination, Navigation } from 'swiper';
-import Img from '../../../../../components/Img';
+import Swiper from '../../../../../components/Swiper';
 
 type CupCardProps = Omit<Cup, 'categoryId' | 'materialTypeId' | 'colorId'>;
-
-const Swiper = styled(SwiperJS)({
-  height: 250,
-});
 
 const CupCard: React.FC<CupCardProps> = ({
  id, title, description, images, price,
 }) => (
   <Grid item xs={12} sm={6} md={4} lg={3}>
-    <Paper elevation={3} sx={{ width: 400 }}>
-      <Swiper pagination={{ dynamicBullets: true }} navigation modules={[Pagination, Navigation]}>
-        {images.map((imgSrc) => (
-          <SwiperSlide key={imgSrc}>
-            <Img src={imgSrc} sx={{ height: '100%', width: '100%' }} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <Paper elevation={3} sx={{ height: 400, maxWidth: 300 }}>
+      <Swiper images={images} />
       <Box sx={{ p: 3 }}>
-        {id}
-        {title}
+        <Typography component="h2" variant="h6">
+          {title}
+        </Typography>
+        <Box>
+          <Button variant="contained" onClick={() => console.log(id)}>
+            Open
+          </Button>
+          <Button variant="contained" onClick={() => console.log(id)}>
+            Add to Cart
+          </Button>
+        </Box>
         {description}
-        {price}
+        <Typography component="p" variant="body1" sx={{ textAlign: 'right' }}>
+          {price}
+          $
+        </Typography>
       </Box>
     </Paper>
   </Grid>
